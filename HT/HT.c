caa56df5547ad080c0c534a1a2a9992cf44d7c18
KeMaKone <atpgamer@gmail.com>
2021-11-08 18:53:37 +0200

    file inits for week8 and ht

---
+++ HT/HT.c
@@ -0,0 +1,24 @@
+//Tekijä Konsta Keski-Mattinen ZZZZ.YY.XX
+/*
+* Lukee tiedostosta avaimet
+* Lisää luetut avaimet tasapainotettuun binääriseen hakupuuhun (linkitetty lista)
+* Tulostaa syntyneen puurakenteen
+* Hakee avaimen mukaisen tiedon puusta (hakee avaimen)
+* Lisää yksittäisiä avaimia puuhun
+*/
+
+
+/* Driver
+* Testaa ohjelmaasi, kun avaimia on 10, 100, 1000, 10000, 100000 (tai vielä enemmän).
+* 2, 4, 6, 8, 10, 12, 14, 30, 28
+* Löytyykö puusta avaimet 6, 1, 10 ja 16?
+* 2, 4, 6, 8, 10, 12, 14, 30, 28 + 26, 24, 22, 20, 18, 16
+* Löytyykö avaimet 10? ja 26? Entäpä avain 32?
+*/
+
+
+/* Tulostus
+* Puun rakentuminen vaihe vaiheelta
+* Haku ilmoittaa löytyykö vai ei
+* puun tulostus sivuttain.
+*/
