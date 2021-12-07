beaccc12ca86ecfddd71bd479d9647b84784c321
KeMaKone <atpgamer@gmail.com>
2021-12-07 18:48:57 +0200

    added time complexity to main

--- HT/HT.c
+++ HT/HT.c
@@ -11,6 +11,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h> 
 
 typedef struct puusolmu_t {
   int luku;
@@ -23,6 +24,9 @@ void oikea_kierto(puuosoitin *, int *);
 void vasen_kierto(puuosoitin *, int *);
 void tulosta_puu(puuosoitin);
 void hae_solmu(puuosoitin *, int);
+//Helperfunctions for main
+void generateList(int [], int);
+void timeSearch(int [], int , puuosoitin *);
 
 /* NOTES
  * puuosoitin *emo = pointer to rootnode
@@ -159,43 +163,77 @@ void hae_solmu(puuosoitin *emo, int haettuArvo){
 }
 
 
+//helper functions for main()
+void generateList(int array[], int len){
+    for(int i=0; i<len; i++){
+        array[i] = rand();
+    }
+}
+
+void timeSearch(int searchArray[], int len, puuosoitin *emo){
+    clock_t begin = clock();
+    for(int i = 0; i < len; i++){
+        hae_solmu(emo, searchArray[i]);
+    }
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Time spent searching %d keys from the current tree of size 15 was %f", len, time_spent);
+}
+
+
 /* Driver
-* Testaa ohjelmaasi, kun avaimia on 10, 100, 1000, 10000, 100000 (tai vielä enemmän).
+* 
 * Löytyykö puusta avaimet 6, 1, 10 ja 16?
 * Löytyykö avaimet 10? ja 26? Entäpä avain 32?
 */
 int main(){
-  int etp = 0, i, luvut0[] = {2, 3, 4, 6, 8, 10, 12, 14, 30, 28, 0}, haettavat0[] = {6, 1, 10, 16, 0}, 
+    int etp = 0, i, luvut0[] = {2, 3, 4, 6, 8, 10, 12, 14, 30, 28, 0}, haettavat0[] = {6, 1, 10, 16, 0}, 
                   luvut1[] = {26, 24, 22, 20, 18, 16, 0},            haettavat1[] = {10, 26, 32, 0};
-  puuosoitin puu = NULL;
+    puuosoitin puu = NULL;
   
-  //Lisätään luvut 2, 4, 6, 8, 10, 12, 14, 30, 28
-  for(i = 0; luvut0[i] != 0; i++){
-    lisaa_solmu(&puu, luvut0[i], &etp);
-    tulosta_puu(puu); // Tulostus: Puun rakentuminen vaihe vaiheelta
+    //Lisätään luvut 2, 4, 6, 8, 10, 12, 14, 30, 28
+    for(i = 0; luvut0[i] != 0; i++){
+        lisaa_solmu(&puu, luvut0[i], &etp);
+        tulosta_puu(puu); // Tulostus: Puun rakentuminen vaihe vaiheelta
+        printf("\n");
+    }
     printf("\n");
-  }
-  printf("\n");
 
-  //Löytyykö puusta avaimet 6, 1, 10 ja 16?
-  for(i = 0; haettavat0[i] != 0; i++){
-    hae_solmu(&puu, haettavat0[i]);
-  }
-  printf("\n");
-
-  //Lisätään luvut 26, 24, 22, 20, 18, 16
-  for(i = 0; luvut1[i] != 0; i++){
-    lisaa_solmu(&puu, luvut1[i], &etp);
-    tulosta_puu(puu); // Tulostus: Puun rakentuminen vaihe vaiheelta
+    //Löytyykö puusta avaimet 6, 1, 10 ja 16?
+    for(i = 0; haettavat0[i] != 0; i++){
+        hae_solmu(&puu, haettavat0[i]);
+    }
     printf("\n");
-  }
-  printf("\n");
 
-  //Löytyykö puusta avaimet 6, 1, 10 ja 16?
-  for(i = 0; haettavat1[i] != 0; i++){
-    hae_solmu(&puu, haettavat1[i]);
-  }
-  printf("\n");
+    //Lisätään luvut 26, 24, 22, 20, 18, 16
+    for(i = 0; luvut1[i] != 0; i++){
+        lisaa_solmu(&puu, luvut1[i], &etp);
+        tulosta_puu(puu); // Tulostus: Puun rakentuminen vaihe vaiheelta
+        printf("\n");
+    }
+    printf("\n");
 
-  return 0;
+    //Löytyykö puusta avaimet 6, 1, 10 ja 16?
+    for(i = 0; haettavat1[i] != 0; i++){
+        hae_solmu(&puu, haettavat1[i]);
+    }
+    printf("\n");
+
+
+
+    printf("\n");
+    //Testaa ohjelmaasi, kun avaimia on 10, 100, 1000, 10000, 100000 (tai vielä enemmän).
+    int search10[10], search100[100], search1000[1000], search10000[10000], search100000[100000];
+    generateList(search10, 10);
+    generateList(search100, 100);
+    generateList(search1000, 1000);
+    generateList(search10000, 10000);
+    generateList(search100000, 100000);
+    timeSearch(search10, 10, &puu);
+    timeSearch(search100, 100, &puu);
+    timeSearch(search1000, 1000, &puu);
+    timeSearch(search10000, 10000, &puu);
+    timeSearch(search100000, 10000, &puu);
+    printf("\n");
+    return 0;
 }
