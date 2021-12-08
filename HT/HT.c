bd5ada227d7e712104b3c68485341308e33fa5fe
KeMaKone <atpgamer@gmail.com>
2021-12-08 16:18:52 +0200

    added bugfixes

--- HT/HT.c
+++ HT/HT.c
@@ -30,6 +30,7 @@ void generateList(int [], int);
 void addListToTree(puuosoitin *, int *, int []);
 void searchListFromTree(puuosoitin *, int [], int);
 void timeSearch(puuosoitin *, int [], int);
+void treeSize(puuosoitin *, int *);
 
 /* NOTES
  * puuosoitin *emo = pointer to rootnode
@@ -158,7 +159,7 @@ void tulosta_puu(puuosoitin solmu, int depth){
   tulosta_puu(solmu->oikea, depth +1);
   printf("\n");
   for(int i = 0; i<depth; i++){
-	  printf(" ");
+	  printf("\t");
   }
   printf("%d\n", solmu->luku);
   printf("\n");
@@ -177,19 +178,21 @@ void hae_solmu(puuosoitin emo, int haettuArvo, int printBool){
       return;
     }
     hae_solmu(emo->oikea, haettuArvo, printBool);
+	return;
   }else if(emo->luku > haettuArvo){
     if((void*) emo->vasen == NULL){
       if(printBool) printf("Valitettavasti lukua %d ei tästä puusta löytynyt\n", haettuArvo);
       return;
     }
     hae_solmu(emo->vasen, haettuArvo, printBool);
+	return;
   } 
   if(printBool) printf("Haettu arvo %d löytyi puusta\n", haettuArvo);
   return;
 }
 
 
-//helper functions for main()
+//helper functions
 void generateList(int array[], int len){
     for(int i=0; i<len; i++){
         array[i] = rand();
@@ -199,7 +202,10 @@ void generateList(int array[], int len){
 void addListToTree(puuosoitin *puu, int *etp, int array[]){
 	for(int i = 0; array[i] != 0; i++){
       lisaa_solmu(puu, array[i], etp);
+	  printf("\n");
+	  printf("Puu alkaa\n");
       tulosta_puu(*puu, 0); // Tulostus: Puun rakentuminen vaihe vaiheelta
+	  printf("Puu loppuu\n");
       printf("\n");
     }
 }
@@ -215,7 +221,13 @@ void timeSearch(puuosoitin *emo, int searchArray[], int len){
     searchListFromTree(emo, searchArray, 0);
     clock_t end = clock();
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Time spent searching %d keys from the current tree of size 15 was %f\n", len, time_spent);
+	int size = 0;
+	treeSize(emo, &size);
+    printf("Time spent searching %d keys from the current tree of size %d was %f\n", len, size, time_spent);
+}
+
+void treeSize(puuosoitin *emo, int* size){
+
 }
 
 
@@ -236,19 +248,21 @@ int main(){
     //Löytyykö puusta avaimet 6, 1, 10 ja 16?
     searchListFromTree(&puu, haettavat0, 1);
     printf("\n");
-
+	
     //Lisätään luvut 26, 24, 22, 20, 18, 16
-    addListToTree(&puu, &etp, luvut0);
+    addListToTree(&puu, &etp, luvut1);
     printf("\n");
 
     //Löytyykö puusta avaimet 6, 1, 10 ja 16?
     searchListFromTree(&puu, haettavat1, 1);
     printf("\n");
-
+	
     printf("\n");
     //Testaa ohjelmaasi, kun avaimia on 10, 100, 1000, 10000, 100000 (tai vielä enemmän).
-    int search10[10], search100[100], search1000[1000], search10000[10000], search100000[100000];
-    generateList(search10, 10);
+    int newKeys[100], search10[10], search100[100], search1000[1000], search10000[10000], search100000[100000];
+    generateList(newKeys, 100);
+	addListToTree(&puu, &etp, newKeys);
+	generateList(search10, 10);
     generateList(search100, 100);
     generateList(search1000, 1000);
     generateList(search10000, 10000);
