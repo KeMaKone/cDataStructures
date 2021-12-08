410c86f461a6f6d7bd8a14cd424cd0b1b1e6eb06
KeMaKone <atpgamer@gmail.com>
2021-12-08 20:37:31 +0200

    changed structure

--- HT/HT.c
+++ HT/HT.c
@@ -1,4 +1,4 @@
-//Tekijä Konsta Keski-Mattinen ZZZZ.YY.XX
+//Tekijä Konsta Keski-Mattinen 2021.12.08
 
 /*
 * Lukee tiedostosta avaimet
@@ -24,13 +24,14 @@ void oikea_kierto(puuosoitin *, int *);
 void vasen_kierto(puuosoitin *, int *);
 void tulosta_puu(puuosoitin, int);
 void hae_solmu(puuosoitin, int, int);
-void readFromFile(int []);
-void findFile(char *);
 void generateList(int [], int);
 void addListToTree(puuosoitin *, int *, int [], int);
 void searchListFromTree(puuosoitin *, int [], int);
 void timeSearch(puuosoitin *, int [], int);
 int treeSize(puuosoitin);
+void readFromFile(int []);
+void findFile(char *);
+int menu();
 
 /* NOTES
  * puuosoitin *emo = pointer to rootnode
@@ -188,32 +189,6 @@ void hae_solmu(puuosoitin emo, int haettuArvo, int printBool){
   return;
 }
 
-void readFromFile(int array[]){
-	char fileName[32];
-    FILE *pFile;
-	int readValue, i = 0;
-
-	findFile(fileName);
-
-	if((pFile = fopen(fileName, "r")) == NULL){
-        perror("Error while reading file");
-        return;
-    }
-	fscanf(pFile, "%*s\n");  // luetaan otsikko
-	printf("Reading file... (MAX 1000000)\n");
-	while(fscanf(pFile, "%d\n", &readValue)){
-		array[++i] = readValue;
-	}
-	array[i] = 0;
-	fclose(pFile);
-}
-
-void findFile(char *target){
-    fflush(stdin); //tyhjennetään stdin
-    printf("Give filename: ");
-    scanf("%s", target);
-}
-
 void generateList(int array[], int len){
     for(int i=0; i<len; i++){
         array[i] = rand();
@@ -243,9 +218,9 @@ void timeSearch(puuosoitin *emo, int searchArray[], int len){
     clock_t begin = clock();
     searchListFromTree(emo, searchArray, 0);
     clock_t end = clock();
-    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    clock_t time_spent = (end - begin);
 	int size = treeSize(*emo);
-    printf("Time spent searching %d keys from the current tree of size %d was %f\n", len, size, time_spent);
+    printf("Time spent searching %d keys from the current tree of size %d was %ld\n", len, size, time_spent);
 }
 
 int treeSize(puuosoitin emo){
@@ -253,22 +228,41 @@ int treeSize(puuosoitin emo){
 	return 1+treeSize(emo->vasen)+treeSize(emo->oikea);
 }
 
-void menu(){
-	char searchAns = '_';
-	int array[1000000];
-	for(int i =0; i<1000000;i++) array[i]=0;
+void readFromFile(int array[]){
+	char fileName[32];
+    FILE *pFile;
+	int i = 0;
+
+	findFile(fileName);
+
+	if((pFile = fopen(fileName, "r")) == NULL){
+        perror("Error while reading file");
+        return;
+    }
+	printf("Reading file... (MAX 100000)\n");
+	while(fscanf(pFile, "%d", &array[i]) != EOF){
+		i++;
+	}
+	array[i] = 0;
+  array[i+1] ='\0';
+	fclose(pFile);
+}
+
+void findFile(char *target){
+    fflush(stdin); //tyhjennetään stdin
+    printf("Give filename: ");
+    scanf("%s", target);
+}
+
+int menu(){
+	char searchAns = '0';
 
 	fflush(stdin); //tyhjennetään stdin
     printf("Add tree from file Y/n? ");
     scanf("%c", &searchAns);
 
-	if(searchAns == 'Y' || searchAns == 'y'){
-		int etp=0;
-		puuosoitin puu = NULL;
-		readFromFile(array);
-		addListToTree(&puu, &etp, array, 1);
-	}
-	return;
+	if(searchAns == 'Y' || searchAns == 'y') return 1;
+	return 0;
 }
 
 
@@ -278,44 +272,53 @@ void menu(){
 * Löytyykö avaimet 10? ja 26? Entäpä avain 32?
 */
 int main(){
-    int etp = 0,  luvut0[] = {2, 3, 4, 6, 8, 10, 12, 14, 30, 28, 0}, haettavat0[] = {6, 1, 10, 16, 0}, 
-                  luvut1[] = {26, 24, 22, 20, 18, 16, 0},            haettavat1[] = {10, 26, 32, 0};
-    puuosoitin puu = NULL;
+	puuosoitin puu = NULL;
+    int etp = 0;
+    int menuChoise = menu();
 
-    //Lisätään luvut 2, 4, 6, 8, 10, 12, 14, 30, 28
-    addListToTree(&puu, &etp, luvut0, 1);
-    printf("\n");
-
-    //Löytyykö puusta avaimet 6, 1, 10 ja 16?
-    searchListFromTree(&puu, haettavat0, 1);
-    printf("\n");
-
+    int luvut0[] = {2, 3, 4, 6, 8, 10, 12, 14, 30, 28, 0}, haettavat0[] = {6, 1, 10, 16, 0}, 
+        luvut1[] = {26, 24, 22, 20, 18, 16, 0},            haettavat1[] = {10, 26, 32, 0};
 	
-    //Lisätään luvut 26, 24, 22, 20, 18, 16
-    addListToTree(&puu, &etp, luvut1, 1);
-    printf("\n");
+	if(menuChoise){
+		int array[100000];
+		for(int i =0; i<100000;i++) array[i]=0;
+		readFromFile(array);
+		for(int i=0; array[i] != 0;i++){
+			printf("%d ", array[i]);
+		}
+		addListToTree(&puu, &etp, array, 0);
+		tulosta_puu(puu, 0);
+	} else {
+		//Lisätään luvut 2, 4, 6, 8, 10, 12, 14, 30, 28
+		addListToTree(&puu, &etp, luvut0, 1);
+		printf("\n");
+  }
+  //Löytyykö puusta avaimet 6, 1, 10 ja 16?
+  searchListFromTree(&puu, haettavat0, 1);
+  printf("\n");
 
-    //Löytyykö puusta avaimet 6, 1, 10 ja 16?
-    searchListFromTree(&puu, haettavat1, 1);
-    printf("\n");
+  //Lisätään luvut 26, 24, 22, 20, 18, 16
+  addListToTree(&puu, &etp, luvut1, 1);
+  printf("\n");
+
+  //Löytyykö puusta avaimet 6, 1, 10 ja 16?
+  searchListFromTree(&puu, haettavat1, 1);
+  printf("\n");
+
+  printf("\n");
+  //Testaa ohjelmaasi, kun avaimia on 10, 100, 1000, 10000, 100000 (tai vielä enemmän).
+  int search10[10], search100[100], search1000[1000], search10000[10000], search100000[100000];
+  generateList(search10, 10);
+  generateList(search100, 100);
+  generateList(search1000, 1000);
+  generateList(search10000, 10000);
+  generateList(search100000, 100000);
+  timeSearch(&puu, search10, 10);
+  timeSearch(&puu, search100, 100);
+  timeSearch(&puu, search1000, 1000);
+  timeSearch(&puu, search10000, 10000);
+  timeSearch(&puu, search100000, 100000);
+  printf("\n");
 	
-    printf("\n");
-    //Testaa ohjelmaasi, kun avaimia on 10, 100, 1000, 10000, 100000 (tai vielä enemmän).
-    int newKeys[200], search10[10], search100[100], search1000[1000], search10000[10000], search100000[100000];
-    generateList(newKeys, 200);
-	addListToTree(&puu, &etp, newKeys, 0);
-	generateList(search10, 10);
-    generateList(search100, 100);
-    generateList(search1000, 1000);
-    generateList(search10000, 10000);
-    generateList(search100000, 100000);
-    timeSearch(&puu, search10, 10);
-    timeSearch(&puu, search100, 100);
-    timeSearch(&puu, search1000, 1000);
-    timeSearch(&puu, search10000, 10000);
-    timeSearch(&puu, search100000, 100000);
-    printf("\n");
-
-	menu();
-    return 0;
+  return 0;
 }
